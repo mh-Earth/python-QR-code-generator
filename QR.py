c86da5aac5b94578007b4c128ea979079eccdc00
mh-Earth <meherab0608@gmail.com>
2020-11-28 04:38:29 -0800

    Initial commit

--- QR.py
+++ QR.py
@@ -20,11 +20,11 @@ def get_QR():
 if __name__ == "__main__":
         
     root=Tk()
-    root.geometry("500x500")
+    root.geometry("500x100")
     root.title("QR code generator")
     lable=Label(root,text="Enter the data for QR code").pack()
     var=StringVar()
-    entry=Entry(root,textvariable=var).pack(ipadx=50)
+    entry=Entry(root,textvariable=var).pack(ipadx=170)
     button=Button(root,text="GET QR CODE",command=get_QR).pack()
 
 
